bf44b2379d7787d92ca7a8ad3adb17ad485b87fd
Daphne <remalidaphne@gmail.com>
2018-02-26 11:20:44 +0100

    add current-step component to trace-segment lib

---
+++ libs/trace-segment/src/current-step/current-step.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'current-step',
+  templateUrl: './current-step.component.html',
+  styleUrls: ['./current-step.component.scss']
+})
+export class CurrentStepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
