b55ff70e841131429e897556fd0a8e5c90ec66be
Daphne <remalidaphne@gmail.com>
2018-03-26 16:36:02 +0200

    fix sub-header component and add tests

--- libs/sub-header/src/sub-header/sub-header.component.ts
+++ libs/sub-header/src/sub-header/sub-header.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { isUndefined } from 'util';
 
 @Component({
   selector: 'sub-header',
@@ -9,11 +8,13 @@ import { isUndefined } from 'util';
   styleUrls: ['./sub-header.component.scss']
 })
 export class SubHeaderComponent implements OnInit {
-  @Input() tableView: boolean;
-  @Input() link: string;
   @Input() goBack: boolean;
+  @Input() changeView: boolean;
 
-  public changeView = false;
+  @Input() link: string;
+  @Input() tableView: boolean;
+  @Input() title: string;
+
   public subTitle = '';
 
   constructor(
@@ -21,30 +22,27 @@ export class SubHeaderComponent implements OnInit {
     private route: ActivatedRoute,
     private titleService: Title
   ) {
-    this.router.events
-      .filter(event => event instanceof NavigationEnd)
-      .subscribe(() => {
-        let currentRoute = this.route.root;
-        do {
-          const childrenRoutes = currentRoute.children;
-          currentRoute = null;
-          childrenRoutes.forEach(routes => {
-            if (routes.outlet === 'primary') {
-              this.subTitle = routes.snapshot.data.title;
-              currentRoute = routes;
-            }
-          });
-        } while (currentRoute);
-        if (this.subTitle !== undefined) {
-          this.titleService.setTitle(this.subTitle);
-        }
-      });
+      this.router.events
+        .filter(event => event instanceof NavigationEnd)
+        .subscribe(() => {
+          let currentRoute = this.route.root;
+          do {
+            const childrenRoutes = currentRoute.children;
+            currentRoute = null;
+            childrenRoutes.forEach(routes => {
+              if (routes.outlet === 'primary') {
+                this.subTitle = routes.snapshot.data.title;
+                currentRoute = routes;
+              }
+            });
+          } while (currentRoute);
+          if (this.subTitle !== undefined) {
+            this.titleService.setTitle(this.subTitle);
+          }
+        });
   }
 
   ngOnInit() {
-    if (this.subTitle === 'Numérisation') {
-      this.changeView = true;
-    }
   }
 
 }
