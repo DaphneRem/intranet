d7f060622120db0c0f0c3153e41127e76815b047
Daphne <remalidaphne@gmail.com>
2018-03-26 15:59:03 +0200

    fix custom-datatables lib and add tests

--- libs/custom-datatables/index.ts
+++ libs/custom-datatables/index.ts
@@ -1,4 +1,2 @@
 export { CustomDatatablesModule } from './src/custom-datatables.module';
-
 export { CustomDatatablesOptions } from './src/models/custom-datatables-options';
-export { Theme } from './src/models/custom-datatables-themes';
