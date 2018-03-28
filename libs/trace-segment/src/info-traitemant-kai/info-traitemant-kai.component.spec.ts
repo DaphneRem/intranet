35bb26292d14011001ec658eef16c77a0689ac0f
Daphne <remalidaphne@gmail.com>
2018-03-28 14:56:38 +0200

    add tests to spec files for trace-segment lib

--- libs/trace-segment/src/info-traitemant-kai/info-traitemant-kai.component.spec.ts
+++ libs/trace-segment/src/info-traitemant-kai/info-traitemant-kai.component.spec.ts
@@ -1,18 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, fakeAsync } from '@angular/core/testing';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { InfoTraitemantKaiComponent } from './info-traitemant-kai.component';
+import { InformationsKaiService } from '../services/informations-kai.service';
 
 describe('InfoTraitemantKaiComponent', () => {
   let testHostFixture: ComponentFixture<TestHostComponent>;
   let testHostComponent: TestHostComponent;
+
   let component: InfoTraitemantKaiComponent;
+  let fixture: ComponentFixture<InfoTraitemantKaiComponent>;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [InfoTraitemantKaiComponent, TestHostComponent],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        imports: [
+          HttpClientModule,
+          HttpClientTestingModule
+        ],
+        declarations: [
+          InfoTraitemantKaiComponent,
+          TestHostComponent
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [InformationsKaiService]
       }).compileComponents();
     })
   );
@@ -25,6 +38,36 @@ describe('InfoTraitemantKaiComponent', () => {
   it('should create component', () => {
     expect(testHostFixture).toBeDefined();
   });
+
+  it('should have minHeight property to 155', () => {
+    fixture = TestBed.createComponent(InfoTraitemantKaiComponent);
+    component = fixture.componentInstance;
+    expect(component.minHeight).toBe(155);
+  });
+
+  it('should call getSupportSegment() function ngOnInit', fakeAsync( () => {
+    fixture = TestBed.createComponent(InfoTraitemantKaiComponent);
+    component = fixture.componentInstance;
+    spyOn(component, 'getSupportSegment');
+    component.ngOnInit();
+    fixture.whenStable().then(() => {
+      expect(component.getSupportSegment).toHaveBeenCalled();
+    });
+  }));
+
+  it('should issue a request', async(
+      inject(
+        [HttpClient, HttpTestingController],
+        (http: HttpClient, backend: HttpTestingController) => {
+          http.get('/foo/bar').subscribe();
+          backend.expectOne({
+            url: '/foo/bar',
+            method: 'GET'
+          });
+        }
+      )
+    )
+  );
 
   @Component({
     selector: `host-component`,
--- libs/trace-segment/src/trace-segment/trace-segment.component.spec.ts
+++ libs/trace-segment/src/trace-segment/trace-segment.component.spec.ts
@@ -1,16 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TraceSegmentComponent } from './trace-segment.component';
 
 describe('TraceSegmentComponent', () => {
   let testHostFixture: ComponentFixture<TestHostComponent>;
   let testHostComponent: TestHostComponent;
+
   let component: TraceSegmentComponent;
+  let fixture: ComponentFixture<TraceSegmentComponent>;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
+        imports: [RouterTestingModule],
         declarations: [TraceSegmentComponent, TestHostComponent],
         schemas: [CUSTOM_ELEMENTS_SCHEMA]
       }).compileComponents();
@@ -25,6 +29,31 @@ describe('TraceSegmentComponent', () => {
   it('should create component', () => {
     expect(testHostFixture).toBeDefined();
   });
+
+  it('should have title property "details fichier"', () => {
+    fixture = TestBed.createComponent(TraceSegmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.title).toBe('details fichier');
+  });
+
+  it('should have file object', () => {
+    fixture = TestBed.createComponent(TraceSegmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.file).toBeDefined();
+    expect(typeof component.file).toBe('object');
+  });
+
+  it('should call checkFilling() function ngOnInit', fakeAsync( () => {
+    fixture = TestBed.createComponent(TraceSegmentComponent);
+    component = fixture.componentInstance;
+    spyOn(component, 'checkFilling');
+    component.ngOnInit();
+    fixture.whenStable().then(() => {
+      expect(component.checkFilling).toHaveBeenCalled();
+    });
+  }));
 
   @Component({
     selector: `host-component`,
