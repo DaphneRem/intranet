33a71a0d61fba45f6e31b76d046072b243a6818b
Daphne <remalidaphne@gmail.com>
2018-03-28 15:29:03 +0200

    add tests to spec file for scroll-to-top lib

--- libs/scroll-to-top/src/scroll-to-top/scroll-to-top.component.spec.ts
+++ libs/scroll-to-top/src/scroll-to-top/scroll-to-top.component.spec.ts
@@ -1,4 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ScrollToTopComponent } from './scroll-to-top.component';
 
@@ -22,4 +23,23 @@ describe('ScrollToTopComponent', () => {
   it('should create component', () => {
     expect(component).toBeDefined();
   });
+
+  it('should have show property to false', () => {
+    fixture.detectChanges();
+    expect(component.show).toBeDefined();
+    expect(component.show).toBe(false);
+  });
+
+  it('should call scrollToTop() function on back-to-top button click', fakeAsync( () => {
+    component.show = true;
+    fixture.detectChanges();
+    spyOn(component, 'scrollToTop');
+    const scrollClass = fixture.debugElement.query(By.css('.back-to-top'));
+    const scrollBtn = scrollClass.nativeElement;
+    scrollBtn.click();
+    fixture.whenStable().then(() => {
+      expect(component.scrollToTop).toHaveBeenCalled();
+    });
+  }));
+
 });
