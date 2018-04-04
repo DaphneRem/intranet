ed9e3e32405d07f4bcc366f8a504e65d5308db2d
Daphne <remalidaphne@gmail.com>
2018-04-04 17:50:29 +0200

    add e2e tests to app0 (ingests)

--- apps/app0/e2e/app.e2e-spec.ts
+++ apps/app0/e2e/app.e2e-spec.ts
@@ -1,14 +1,67 @@
 import { AppPage } from './app.po';
 
-describe('ng-nx-project App', () => {
+describe('Ingests App (app0)', () => { 
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display app0 tilte', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('App0');
+ // Check title in navigation :
+
+  it('should display title "Numérisation" to path "/"', () => {
+    page.navigateToHome();
+    expect(page.getPageTitle()).toEqual('Numérisation');
   });
+
+  it('should display title "Numérisation" to path "ingests/tables-view"', () => {
+    page.navigateToHomeTable();
+    expect(page.getPageTitle()).toEqual('Numérisation');
+  });
+
+  it('should display title "Fichiers en cours de traitement" to path "ingests/in-progress"', () => {
+    page.navToIngestsInProgress();
+    expect(page.getPageTitle()).toEqual('Fichiers en cours de traitement');
+  });
+
+  it('should display title "Fichiers Terminés" to path "ingests/completed"', () => {
+    page.navToIngestsCompleted();
+    expect(page.getPageTitle()).toEqual('Fichiers Terminés');
+  });
+
+  it('should display title "En attente KAI" to path "ingests/kai-waiting"', () => {
+    page.navToKaiWaiting();
+    expect(page.getPageTitle()).toEqual('Numérisation KAI');
+  });
+
+  it('should display title "En attente KARINA" to path "ingests/karina-waiting"', () => {
+    page.navToKarinaWaiting();
+    expect(page.getPageTitle()).toEqual('En attente KARINA');
+  });
+
+  it('should display title "Publicité" to path "publicity"', () => {
+    page.navigateToPub();
+    expect(page.getPageTitle()).toEqual('Publicité');
+  });
+
+  it('should display title "Publicité" to path "publicity/tables-view"', () => {
+    page.navigateToPubTable();
+    expect(page.getPageTitle()).toEqual('Publicité');
+  });
+
+  it('should display title "Publicités en cours de traitement" to path "publicity/in-progress"', () => {
+    page.navToPubInProgress();
+    expect(page.getPageTitle()).toEqual('Publicités en cours de traitement');
+  });
+
+  it('should display title "Publicités Terminés" to path "publicity/completed"', () => {
+    page.navToPubCompleted();
+    expect(page.getPageTitle()).toEqual('Publicités Terminés');
+  });
+
+  it('should display title "Fichiers purgés" to path "purged"', () => {
+    page.navigateToPurged();
+    expect(page.getPageTitle()).toEqual('Fichiers purgés');
+  });
+
 });
