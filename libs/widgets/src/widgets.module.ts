3273a3d3cb6bf3d6300d5a1d3357aed6b4cdeaaf
Daphne <remalidaphne@gmail.com>
2018-04-30 14:30:41 +0200

    add WidgetErrorComponent

--- libs/widgets/src/widgets.module.ts
+++ libs/widgets/src/widgets.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 // components imports
 import { WidgetDataComponent } from './widget-data/widget-data.component';
+import { WidgetErrorComponent } from './widget-error/widget-error.component';
 import { WidgetLinkComponent } from './widget-link/widget-link.component';
 
 @NgModule({
@@ -13,10 +14,12 @@ import { WidgetLinkComponent } from './widget-link/widget-link.component';
   ],
   declarations: [
     WidgetDataComponent,
-    WidgetLinkComponent
+    WidgetErrorComponent,
+    WidgetLinkComponent,
   ],
   exports : [
     WidgetDataComponent,
+    WidgetErrorComponent,
     WidgetLinkComponent
   ]
 })
