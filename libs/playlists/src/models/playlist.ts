c32c4150e837b17c1cc23e2581b6e0b133d86c9e
Daphne <remalidaphne@gmail.com>
2018-04-30 11:04:03 +0200

    add playlist model

---
+++ libs/playlists/src/models/playlist.ts
@@ -0,0 +1,13 @@
+export class Playlist {
+
+    action: string;
+    chaine: string;
+    commentaire: string;
+    datediff: string;
+    dateordre: string;
+    depart: string;
+    etat: number;
+    typeplaylist: string;
+    version: number;
+
+}
