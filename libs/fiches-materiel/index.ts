8d58bc4fb2c80f5ce95e24aaef99f622dd606303
Daphne <remalidaphne@gmail.com>
2018-06-18 17:28:15 +0200

    export pages component to fiches-materiel lib

--- libs/fiches-materiel/index.ts
+++ libs/fiches-materiel/index.ts
@@ -1,2 +1,9 @@
 export { FichesMaterielLibModule } from './src/fiches-materiel-lib.module';
+
+// export pages
 export { FichesMaterielCreationComponent } from './src/fiches-materiel-pages/fiches-materiel-creation/fiches-materiel-creation.component';
+export {
+    FichesMaterielWidgetViewComponent
+} from './src/fiches-materiel-pages/fiches-materiel-widget-view/fiches-materiel-widget-view.component';
+export { DisplayFichesAchatsComponent } from './src/fiches-materiel-pages/display-fiches-achats/display-fiches-achats.component';
+export { MyFichesMaterielComponent } from './src/fiches-materiel-pages/my-fiches-materiel/my-fiches-materiel.component';
