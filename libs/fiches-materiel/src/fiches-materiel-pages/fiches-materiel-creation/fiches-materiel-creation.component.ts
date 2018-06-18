ca2c5c9259e37418f5b6f59dc23004f072f2c06d
Daphne <remalidaphne@gmail.com>
2018-06-18 17:29:09 +0200

    fix custom-icons

--- libs/fiches-materiel/src/fiches-materiel-pages/fiches-materiel-creation/fiches-materiel-creation.component.ts
+++ libs/fiches-materiel/src/fiches-materiel-pages/fiches-materiel-creation/fiches-materiel-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomIconBadge } from '@ab/custom-icons';
 
 @Component({
   selector: 'fiches-materiel-creation',
@@ -7,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FichesMaterielCreationComponent implements OnInit {
 
+  public icons = [];
+  public fichesMaterielCreation: CustomIconBadge = {
+      littleIcon : {
+        circleColor: '#3383FF',
+        icon : 'icofont icofont-eye',
+        iconSize: '1.5em',
+        iconMargin: '2px',
+      },
+      bigIcon : {
+        icon: 'icofont icofont-file-text',
+        circleColor:  '#999898',
+      },
+      link : '../material-sheets/my-material-sheets'
+  };
+  public fichesAchatView: CustomIconBadge = {
+      littleIcon : {
+        circleColor: '#3383FF',
+        icon : 'icofont icofont-eye',
+        iconSize: '1.5em',
+        iconMargin: '2px',
+      },
+      bigIcon : {
+        icon: 'icofont icofont-tag',
+        circleColor:  '#999898',
+      },
+      link : '/displaying-purchase-sheets'
+  };
+
   constructor() { }
 
   ngOnInit() {
-    console.log('ok');
+    this.icons = [this.fichesMaterielCreation, this.fichesAchatView];
   }
 
 }
