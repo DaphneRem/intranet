30653c99e2af55c0f4b914b7b81f14c8346a699e
Daphne <remalidaphne@gmail.com>
2018-06-18 17:26:56 +0200

    add dispaly-fiches-achats component to fiches-materiel-lib

---
+++ libs/fiches-materiel/src/fiches-materiel-pages/display-fiches-achats/display-fiches-achats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'display-fiches-achats',
+  templateUrl: './display-fiches-achats.component.html',
+  styleUrls: ['./display-fiches-achats.component.scss']
+})
+export class DisplayFichesAchatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
