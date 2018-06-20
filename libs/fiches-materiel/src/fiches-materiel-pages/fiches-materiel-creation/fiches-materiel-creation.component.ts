07740fe473e0afd8123d5e440dda174e6e5ef8b9
Daphne <remalidaphne@gmail.com>
2018-06-20 09:03:11 +0200

    add fiches-achats-table to fiches-achat-creation component

--- libs/fiches-materiel/src/fiches-materiel-pages/fiches-materiel-creation/fiches-materiel-creation.component.ts
+++ libs/fiches-materiel/src/fiches-materiel-pages/fiches-materiel-creation/fiches-materiel-creation.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
 import { CustomIconBadge } from '@ab/custom-icons';
+import {Component, Input, OnInit } from '@angular/core';
+
+
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'fiches-materiel-creation',
   templateUrl: './fiches-materiel-creation.component.html',
-  styleUrls: ['./fiches-materiel-creation.component.scss']
+  styleUrls: [
+    './fiches-materiel-creation.component.scss'],
 })
 export class FichesMaterielCreationComponent implements OnInit {
+
+  public openModal;
+
+
+  public daysTableView = 3;
+  public headerTableLinkExist = false;
 
   public icons = [];
   public fichesMaterielCreation: CustomIconBadge = {
