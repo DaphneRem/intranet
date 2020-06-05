ad4d3f816e65e67cd1c2da2dd1c0358cacc5c08f
lydiaha <haretlydia@gmail.com>
2020-06-05 17:21:09 +0200

    add btn deplacement multiple

--- libs/k-planner-lib/src/multiselect-workorder/multiselect-workorder.component.ts
+++ libs/k-planner-lib/src/multiselect-workorder/multiselect-workorder.component.ts
@@ -4,17 +4,23 @@ import { TreeViewComponent } from '@syncfusion/ej2-angular-navigations';
 // import { Input } from '@syncfusion/ej2-inputs';
 import { GridComponent } from '@syncfusion/ej2-angular-grids';
 import { Internationalization } from '@syncfusion/ej2-base';
+import { outputs } from '@syncfusion/ej2-angular-popups/src/tooltip/tooltip.component';
 let instance: Internationalization = new Internationalization();
 @Component({
   selector: 'app-multiselect-workorder',
   templateUrl: './multiselect-workorder.component.html',
-  styleUrls: ['./multiselect-workorder.component.scss']
+  styleUrls: ['./multiselect-workorder.component.scss',
+  '../../../../assets/icon/icofont/css/icofont.scss'
+]
 })
 
 export class MultiselectWorkorderComponent implements OnInit {
   @Input() eventSelect: any
-
-  @Output()  selectItemInMulutiselction = new EventEmitter<any>()
+  
+  @Output()  selectItemInMulutiselction = new EventEmitter<any>();
+  @Output() deplacementMultipleEvent =  new EventEmitter<any>();
+  @Output() clickBtnDeplacement =  new EventEmitter<any>();
+  
   @ViewChild('DialogMultiSelect')
   public DialogMultiSelect: DialogComponent;
   @ViewChild('treeObjMultiSelect')
@@ -23,13 +29,13 @@ export class MultiselectWorkorderComponent implements OnInit {
     @ViewChild('grid')
     public grid: GridComponent;
 
-
- public cliqueComptEventMultiSelect : Boolean = false;
- public showCloseIcon: Boolean = true;
-public width: string = '90%';
+  public clickDeplacementMultiple : boolean  = false;
+ public cliqueComptEventMultiSelect : boolean = false;
+ public showCloseIcon: boolean = true;
+public width: string = '95%';
 public animationSettings: AnimationSettingsModel = { effect: 'None' };
 public targetModal: string = '.schedule';
-public header: string = 'Eléments Sélectionnés';
+public header: string
 public data
 
 //listview init 
@@ -65,6 +71,9 @@ public confirmDlgBtnClick = (): void => {
 }
 onCliqueComptEventSelect(){
   this.cliqueComptEventMultiSelect = true;
+  this.clickDeplacementMultiple = false;
+  this.width = '95%';
+  this.header= 'Eléments Sélectionnés';
   console.log("compt click",  this.cliqueComptEventMultiSelect )
   console.log(this.DialogMultiSelect)
 
@@ -85,10 +94,26 @@ onselectItem(args){
 }
 onActionComplete(args){
   console.log('Action complete',args)
+
+
+}
+deplacementMultiple(){
+
+  this.cliqueComptEventMultiSelect = false;
+  this.width = '95%';
+  this.header= 'Sélectionnez une date';
+  this.clickDeplacementMultiple = true;
+
+    setTimeout(() => {
+      console.log(document.getElementsByClassName('.dialog-Class'))
+      this.DialogMultiSelect.show()
+      this.clickBtnDeplacement.emit()
+      console.log(this.DialogMultiSelect)
+    }, 100);
+    
   
 
 }
-
 public format(value: Date): string {
   return instance.formatDate(value, { skeleton: 'yMd', type: 'date' });
 }
@@ -97,7 +122,11 @@ changeDateFormat(date) {
   let newDate = new Date(date);
   return newDate;
 }
-
+onValueChange(args: any):void {
+console.log(args);
+this.deplacementMultipleEvent.emit(args);
+this.DialogMultiSelect.hide();
+}
 }
 
 
